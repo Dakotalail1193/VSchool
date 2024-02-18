c0411a415a94c2d3ff94d4db205101c7f9e28607
dakota <dakotalail1193@gmail.com>
2024-02-18 15:09:08 -0500

    Event Listener

---
+++ Level1/Assignments/Event-Listener/app.js
@@ -0,0 +1,50 @@
+var boxColor = document.getElementById("box")
+
+//boxColor.addEventListener('click', function(){
+//    document.boxColor.style.color = "blue"
+//})
+
+document.getElementById("box").addEventListener('mouseover', function(){
+    boxColor.style.backgroundColor = "blue";
+    
+})
+document.getElementById("box").addEventListener('mouseout', function(){
+    boxColor.style.backgroundColor = "white";
+    
+})
+document.getElementById("box").addEventListener('mousedown', function(){
+    boxColor.style.backgroundColor = "red";
+    
+})
+document.getElementById("box").addEventListener('mouseup', function(){
+    boxColor.style.backgroundColor = "yellow";
+    
+})
+document.getElementById("box").addEventListener('dblclick', function(){
+    boxColor.style.backgroundColor = "green";
+    
+})
+document.getElementById("box").addEventListener('wheel', function(){
+    boxColor.style.backgroundColor = "orange";
+    
+})
+
+document.addEventListener('keydown', function(e){
+    if (e.which === 66) {
+        boxColor.style.backgroundColor = "blue";
+    }
+    else if (e.which === 82) {
+        boxColor.style.backgroundColor = "red";
+}
+    else if (e.which === 89) {
+    boxColor.style.backgroundColor = "yellow";
+}
+    else if (e.which === 79) {
+    boxColor.style.backgroundColor = "orange";
+}
+    else if (e.which === 71) {
+    boxColor.style.backgroundColor = "green";
+}
+});
+
+
