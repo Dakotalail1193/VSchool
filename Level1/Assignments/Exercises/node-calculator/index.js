ddbe7ad36e2e2522bda05ab4f1bf841e2feb2988
Dakota Lail <dakotalail1193@gmail.com>
2024-02-23 10:59:04 -0500

    node calculator

---
+++ Level1/Assignments/Exercises/node-calculator/index.js
@@ -0,0 +1,42 @@
+const readline = require("readline-sync")
+
+const sum = (num1, num2) => {
+ console.log(num1 + num2)
+}
+
+const sub = (num1, num2) => {
+    console.log(num1 - num2)
+}
+
+const div = (num1, num2) => {
+    console.log(num1 / num2)
+}
+
+const mul = (num1, num2) => {
+    console.log(num1 * num2)
+}
+
+const num1 = readline.question ('First Number ')
+
+
+const num2 = readline.question ('Second Number ')
+
+
+const operation = readline.question ('Operation(+, -, /, *) ')
+ 
+
+     if (operation === '+') {
+         sum(num1, num2);         
+    } else if (operation === "-") {
+        sub(num1, num2); 
+    } else if (operation === "/") {
+      div(num1, num2); 
+    } else if (operation === "*"){
+       mul(num1, num2); 
+    }
+ 
+ 
+ 
+
+    
+
