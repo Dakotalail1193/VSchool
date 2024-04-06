9970b53377657bd5b845276e5676e63312ef3371
Dakota Lail <dakotalail1193@gmail.com>
2024-04-05 20:29:49 -0400

    get/post

--- Level2/Challenges/Capstone-Challenge/index.js
+++ Level2/Challenges/Capstone-Challenge/index.js
@@ -1,49 +1,76 @@
 const todoForm = document.form
 const submit = document.getElementById("submit")
 
-// function getData(){
-//     axios.get("https://api.vschool.io/dakotalail/todo")
-//         .then(res => listData(res.data))
-//         .catch(err => console.log(err))
-// }
+function getData(){
+    axios.get("https://api.vschool.io/dakotalail/todo")
+        .then(response => listData(response.data))
+        .catch(error => console.log(error))
+}
 
-// function listData (){
-//     for (let i = 0; i < data.length; i++){
-//         const title = document.createElement('h1');
-//         title.textContent = data[i].title;
-//         title.className = "todo-title";  
-//         document.getElementById('todo-item').appendChild(title);
+function listData (data){
+    for (let i = 0; i < data.length; i++){
+        const title = document.createElement('h1');
+        title.textContent = data[i].title;
+        title.className = "todo-title";  
+        document.getElementById("todo-item").appendChild(title);
 
-//         const details = document.createElement('p');
-//         details.textContent = data[i].details;
-//         details.className = "todo-details";
-//         document.getElementById("todo-title").appendChild(details);
+        const description = document.createElement('p');
+        description.textContent = data[i].description;
+        description.className = "todo-details";
+        document.getElementById("todo-item").appendChild(description);
 
-//         const price = document.createElement('p');
-//         price.textContent = data[i].price;
-//         price.className = "todo-price";
-//         document.getElementById("todo-details").appendChild(price);
+        const price = document.createElement('p');
+        price.textContent = data[i].price;
+        price.className = "todo-price";
+        document.getElementById("todo-item").appendChild(price);        
 
+        const img = document.createElement('img');
+        img.src = data[i].imgUrl;
+        img.className = "todo-img"
+        document.getElementById("todo-item").appendChild(img); 
+
+        const divButton = document.createElement("div");
+        divButton.className = "buttons";
+        document.getElementById("todo-item").appendChild(divButton);
         
+        const edit = document.createElement("button");
+        edit.textContent = "Edit";
+        edit.className = "todo-buttons";
+        divButton.appendChild(edit);
 
-//         const img = documentcreateElement('img');
-//         img.src = data[i].imgUrl;
-//         img.className = "todo-img"
-//         document.getElementById("todo-price").appendChild(img);       
-//     }
-// }
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "todo-buttons";
+        divButton.appendChild(deleteButton);
+
+        const divComplete = document.createElement("div");
+        divComplete.className = "buttons";
+        document.getElementById("todo-item").appendChild(divComplete);
+
+        const label = document.createElement("label");
+        label.textContent = "Completed"
+        divComplete.appendChild(label);
+
+        const check = document.createElement("input");
+        check.type = "checkbox";
+        divComplete.appendChild(check);
+
+    }
+}
+
+getData()
 
 todoForm.addEventListener("submit", function (event){
     event.preventDefault()
 
     const newTodo = {
-        title: todoForm.title.value,
+        item: todoForm.title.value,
         description: todoForm.description.value,
         price: todoForm.price.value,
         imgUrl: todoForm.imgUrl.value
     }
 
     axios.post("https://api.vschool.io/dakotalail/todo", newTodo)
-        .then(response => console.log(response.data))
+        .then(res => getData())
         .catch(error => console.log(error))
 })
