6a2caa8af0a474780cc3730af6dc356029f05423
Dakota Lail <dakotalail1193@gmail.com>
2024-04-15 20:25:28 -0400

    level 3

---
+++ Level3/Practice/scrimba/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './scrimba-componets/Navbar.jsx'
+import Main from './scrimba-componets/Main.jsx'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div> 
+  )
+}
+
+
