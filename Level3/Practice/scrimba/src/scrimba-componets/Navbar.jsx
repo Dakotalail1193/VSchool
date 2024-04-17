9989faa70b0cb7d94d7042e0424e595fc02a6ff3
Dakota Lail <dakotalail1193@gmail.com>
2024-04-16 20:01:06 -0400

    level 3

--- Level3/Practice/scrimba/src/scrimba-componets/Navbar.jsx
+++ Level3/Practice/scrimba/src/scrimba-componets/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Navbar() {
-    return <h1>Navbar goes here </h1>
+    return (
+        <nav>
+            <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" className="nav--logo" />
+            <h3 className="nav--logo_text">ReactFacts </h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+    
 }
---
+++ Level3/Practice/scrimba/src/scrimba-componets/SCApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar.jsx'
+import Main from './Main.jsx'
+
+import './SCApp.css'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div> 
+  )
+}
+
+//ADD THIS TO main.jsx
+//import App from './scrimba-componets/SCApp.jsx'
