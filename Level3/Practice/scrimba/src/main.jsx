4f6ce68dfd4bb68f70839405d5fc8096a90cce16
Dakota Lail <dakotalail1193@gmail.com>
2024-04-21 20:09:54 -0400

    mapping componets

--- Level3/Practice/scrimba/src/main.jsx
+++ Level3/Practice/scrimba/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Airbnb-componets/AbnbApp.jsx'
+import App from '../src/Jokes-componets/JApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
