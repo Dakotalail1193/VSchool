94707bbde95480cce5433baa4602435ecb43a12d
Dakota Lail <dakotalail1193@gmail.com>
2024-05-05 19:50:06 -0400

    state

--- Level3/Practice/scrimba/src/main.jsx
+++ Level3/Practice/scrimba/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from '../src/Practice/PracApp'
+import App from '../src/Counter/CountApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
