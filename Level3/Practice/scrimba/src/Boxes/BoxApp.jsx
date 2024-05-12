6809f8242bf8f74df2e5c961f4bc31c1af79b6d1
Dakota Lail <dakotalail1193@gmail.com>
2024-05-11 21:51:12 -0400

    Box

--- Level3/Practice/scrimba/src/Boxes/BoxApp.jsx
+++ Level3/Practice/scrimba/src/Boxes/BoxApp.jsx
@@ -1,14 +1,27 @@
 import React from "react"
 import boxes from "./componets/boxes"
+import Box from "./componets/Box"
 import "./BoxApp.css"
 
-export default function App(props){
+export default function App(){
     const [box, setBox] = React.useState(boxes)
-    const styles ={
-        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+
+    function toggle(id){
+     setBox(prevBox => {
+        return prevBox.map((box) => {
+           return box.id === id ? {...box, on: !box} : box
+        })
+
+        })
     }
+    
     const boxElements = box.map(box => (
-        <div style={styles} className="box" key={box.id}></div>
+        <Box
+         key={box.id}
+         id={box.id}
+         on={box.on}
+         toggle={toggle} 
+          />
     ))
 
     return (
--- Level3/Practice/scrimba/src/main.jsx
+++ Level3/Practice/scrimba/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './DJ/DjApp.jsx'
+import App from './Boxes/BoxApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
