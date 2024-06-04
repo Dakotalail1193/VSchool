64bf886a9793d46f3aeebc578d6124c4bc6d3570
Dakota Lail <dakotalail1193@gmail.com>
2024-06-04 19:05:54 -0400

    Cipher

--- Level3/Challenge/Cipher/index.js
+++ Level3/Challenge/Cipher/index.js
@@ -1,17 +1,26 @@
-var readline = require('readline-sync');
-var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
-var shift = parseInt(readline.question('How many letters would you like to shift? '));
+const readline = require('readline-sync');
+const input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
+const shift = parseInt(readline.question('How many letters would you like to shift? '));
 
-function ceasarCipher (string, shift) {
-    let resultArray = []
-    for ( i=0; i < string.length; i++) {
-        let code = string.charCodeAt(i) + shift
-        while (code < 122) {
-            code = (code - 122) + 96
-        }
-        resultArray.push(string.fromCharCode(code))
+function caesarCipher(str, shift) {
+    var output = "";
+     
+    // Loop through each character in the input string
+    for (var i = 0; i < str.length; i++) {
+      var code = str[i].charCodeAt();
+       
+      // Check if the character is a letter
+      if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        // Shift the letter by the shift amount, wrapping around the alphabet if necessary
+        code += shift;
+      }
+       
+      // Add the new character to the output string
+       output += String.fromCharCode(code);
     }
-        return resultArray.join("")
-}
+     
+    // Return the output string
+    return output;
+  }
 
-console.log(ceasarCipher())
+console.log(caesarCipher(input, shift))
