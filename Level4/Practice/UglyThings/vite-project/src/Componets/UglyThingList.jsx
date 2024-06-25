d20a646b866a1ebb96b8b3c34a31f61bd72a4fda
Dakota Lail <dakotalail1193@gmail.com>
2024-06-25 19:56:58 -0400

    ugly styling

--- Level4/Practice/UglyThings/vite-project/src/Componets/UglyThingList.jsx
+++ Level4/Practice/UglyThings/vite-project/src/Componets/UglyThingList.jsx
@@ -7,7 +7,7 @@ function UglyThingList(){
     const {uglyArray, handleDelete, saveEdit} = useContext(UglyContext)
 
     const uglyElement = uglyArray.map((uglyThing, index) => {
-        return <UglyThing key = {index} {...uglyThing} index = {index} onSave = { () => saveEdit(index)} onDelete = { () => handleDelete(index)}/>
+        return <UglyThing key = {index} {...uglyThing} index = {index} onSave = { () => saveEdit(index)} onDelete = { () => handleDelete(index)} className="ugly-thing"/>
     })
     return(
         <>
