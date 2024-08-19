0b3a4f99bbe813eae972b3081eb676d1b099315d
Dakota Lail <dakotalail1193@gmail.com>
2024-08-18 20:49:02 -0400

    backend

--- Level6/Capstone/routes/authRouter.js
+++ Level6/Capstone/routes/authRouter.js
@@ -20,4 +20,24 @@ authRouter.post('/signup', async(req, res, next) => {
     }
 })
 
+authRouter.post('/login', async (req, res, next) => {
+    try {
+        const user = await User.findOne({username:req.body.username})
+        if(!user){
+            res.status(403)
+            return next(new Error('Incorrect Username or Password'))
+        }
+        const passwordCheck = await user.checkPassword(req.body.password)
+        if(!passwordCheck){
+            res.status(403)
+            return next(new Error('Incorrect Username or Password'))
+        }
+        const token =jwt.sign(user.toObject(), process.env.SECRET)
+        return res.status(201).send({user, token})
+    } catch (error) {
+        res.status(500)
+        return next(error)
+    }
+})
+
 module.exports = authRouter
